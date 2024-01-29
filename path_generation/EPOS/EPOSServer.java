75ec522601106bff3dd9161cb5d2e71a8ff42a08
C41Underscore <callum.methold@sky.com>
2024-01-29 13:33:37 +0000

    EPOSServer.java and EPOSServer.py improved

--- path_generation/EPOS/EPOSServer.java
+++ path_generation/EPOS/EPOSServer.java
@@ -1,5 +1,6 @@
 package EPOS;
 
+
 import py4j.GatewayServer;
 
 //  TODO: Compile EPOS classes into single, runnable jar
@@ -8,12 +9,13 @@ import py4j.GatewayServer;
 
 class EPOSServer
 {
-    public String runExperiment() {return "Hello";}
+    public String start() {return "EPOS Starting...";}
+
+    public String stop() {return "EPOS Stopping...";}
 
     public static void main(String[] args)
     {
-        GatewayServer g = new GatewayServer(new EPOSServer());
+        GatewayServer g = new GatewayServer(new EPOSServer(), 8081);
         g.start();
-        System.out.println("EPOS Server started...");
     }
 }
--- path_generation/EPOSServer.py
+++ path_generation/EPOSServer.py
@@ -1,6 +1,6 @@
 from JavaServer import JavaServer
 
-from py4j.java_gateway import JavaGateway
+from py4j.java_gateway import JavaGateway, GatewayParameters
 
 
 class EPOSServer(JavaServer):
@@ -10,12 +10,21 @@ class EPOSServer(JavaServer):
 
     def start(self) -> int:
         """ Start the Gateway Server and load all necessary classes."""
-    pass
+        parameters = GatewayParameters(address="127.0.0.1", port=8081)
+        self.epos_server = JavaGateway(gateway_parameters=parameters).entry_point
+        return 0
 
     def execute(self) -> str:
         """ Execute the desired workflow from the JVM."""
-        pass
+        return self.epos_server.start()
 
     def stop(self) -> int:
         """ Stop the Gateway Server."""
-        pass
+        return self.epos_server.stop()
+
+
+if __name__ == "__main__":
+    epos_server = EPOSServer()
+    epos_server.start()
+    print(epos_server.execute())
+    print(epos_server.stop())
