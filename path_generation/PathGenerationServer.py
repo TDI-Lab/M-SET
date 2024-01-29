76ceaccee6f62ea61b4b52da56d3bb7cdcbb21dc
C41Underscore <callum.methold@sky.com>
2024-01-29 13:30:50 +0000

    PlanGenerationServer.java and PathGenerationServer.py improved

--- path_generation/PathGenerationServer.py
+++ path_generation/PathGenerationServer.py
@@ -1,6 +1,6 @@
 from JavaServer import JavaServer
 
-from py4j.java_gateway import JavaGateway
+from py4j.java_gateway import JavaGateway, GatewayParameters
 
 
 class PathGenerationServer(JavaServer):
@@ -10,12 +10,21 @@ class PathGenerationServer(JavaServer):
 
     def start(self) -> int:
         """ Start the Gateway Server and load all necessary classes."""
-        pass
+        parameters = GatewayParameters(address="127.0.0.1", port=8080)
+        self.pg_server = JavaGateway(gateway_parameters=parameters).entry_point
+        return 0
 
     def execute(self) -> str:
         """ Execute the desired workflow from the JVM."""
-        pass
+        return self.pg_server.start()
 
     def stop(self) -> int:
         """ Stop the Gateway Server."""
-        pass
+        return self.pg_server.stop()
+
+
+if __name__ == "__main__":
+    pg = PathGenerationServer()
+    pg.start()
+    print(pg.execute())
+    print(pg.stop())
--- path_generation/PlanGeneration/PlanGenerationServer.java
+++ path_generation/PlanGeneration/PlanGenerationServer.java
@@ -5,12 +5,13 @@ import py4j.GatewayServer;
 
 class PlanGenerationServer
 {
-    public String runExperiment() {return "Hello";}
+    public String start() {return "Plan Generation Starting...";}
+
+    public String stop() {return "Plan Generation Stopping...";}
 
     public static void main(String[] args)
     {
-        GatewayServer g = new GatewayServer(new PlanGenerationServer());
+        GatewayServer g = new GatewayServer(new PlanGenerationServer(), 8080);
         g.start();
-        System.out.println("Plan Generation Server started...");
     }
 }
