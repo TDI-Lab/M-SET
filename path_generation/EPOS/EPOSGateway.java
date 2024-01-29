e821399a53934bf955e53b8807be2462a7356ada
C41Underscore <callum.methold@sky.com>
2024-01-29 16:36:17 +0000

    removed TODO

--- path_generation/EPOS/EPOSGateway.java
+++ path_generation/EPOS/EPOSGateway.java
@@ -1,16 +1,11 @@
 package EPOS;
 
-
 import py4j.GatewayServer;
-
-//  TODO: Compile EPOS classes into single, runnable jar
-//  TODO: Add Gateway server to EPOS
 
 
 class EPOSGateway
 {
     public String start() {return "EPOS Starting...";}
-
     public String stop() {return "EPOS Stopping...";}
 
     public static void main(String[] args)
