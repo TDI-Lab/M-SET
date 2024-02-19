e7a673353d57c4bb5dfc11c301cb1e4bd4cdd1ff
C41Underscore <callum.methold@sky.com>
2024-02-19 14:14:43 +0000

    updated main

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 """
 Workflow executed from here.
 """
+import numpy as np
+
 from path_generation.PathGenerator import PathGenerator
 
 
 def main():
     pg = PathGenerator()
     plans = pg.generate_paths()
+    plan_total = np.array([0. for _ in range(len(plans[0][1]))])
     for plan in plans:
         print(plan)
+        plan_total += np.array(plan[1])
+    target_total = np.array([5., 9., 4., 5., 4., 8.])
+    print("Total Plans:", plan_total)
+    print("Mismatch:", plan_total - target_total)
 
 
 if __name__ == '__main__':
--- path_generation/EPOS/EPOSWrapper.py
+++ path_generation/EPOS/EPOSWrapper.py
@@ -38,7 +38,6 @@ class EPOSWrapper:
         proc.wait()
         out, err = proc.communicate()
         print(err.decode("utf-8"))
-        print(out.decode("utf-8"))
         return proc.returncode
 
     # Clean the output directory
