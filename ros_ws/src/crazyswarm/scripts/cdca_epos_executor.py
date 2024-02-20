42b2769f521c372b8a3f2a8116f4029499894d6d
Adam <sc19ap>
2024-02-20 19:27:19 +0000

    Deleting unused lines

--- ros_ws/src/crazyswarm/scripts/cdca_epos_executor.py
+++ ros_ws/src/crazyswarm/scripts/cdca_epos_executor.py
@@ -1,20 +1,14 @@
 import numpy as np
-import sys
 
 # Add chdir here to change to directory containing crazyswarm installation
 
 from pycrazyswarm import Crazyswarm
-import time
 
 swarm = Crazyswarm()
 timeHelper = swarm.timeHelper
 allcfs = swarm.allcfs
 
-input = [[[[1,2],3],[[4,5],6]],[[[7,8],9],[[10,11],12]]]
-input = [[[[0,2],3],[[4,5],6]],[[[7,8],9],[[10,11],12]]]
-#input = [[[[0,2],5],[[4,5],7]]]
-
-print(input)
+input = [[[[0,0],3],[[1,1],6]],[[[2,1],9],[[1,0],12]]]
 
 # Translate the positions on the testbed to coordinates ((0,0) as the centre of the testbed)
 position_to_coords = {
@@ -28,11 +22,12 @@ position_to_coords = {
     "[2.5,1.5]": [0.8299, 0.47] # top right corner of testbed
 }
 
-class Drone:
-    def __init__(self):
+class Drone():
+    def __init__(self, drone):
         self.positions = []
         self.times = [] 
         self.status = "idle" # idle -> (sensing, waiting, moving)
+        self.drone = drone
 
 # Parameters
 all_drones = []
@@ -41,28 +36,30 @@ travel_time = 3
 sensing_time = 1
 
 #parse the input
+c = 0
 for drone in input:
-    d = Drone()
+    d = Drone(allcfs.crazyflies[0])
     all_drones.append(d)
     for position in drone:
-        d.positions.append(position[0])
+        d.positions.append(str(position[0]).replace(' ',''))
         #               set-off time (from previous cell) + travel time + sensing time + wait time
         #d.times.append(d.times[max(0,len(d.times)-1)]    + travel_time + sensing_time + position[1])
         d.times.append(int(position[1]))
     
-    #next_moves = np.append(next_moves, int(drone[0][1])) # Queue the time that the drone waits at the first position
     next_moves = np.append(next_moves, 0) # Queue the time that the drone waits at the first position
 
-    print(d.positions)
-
+    c+=1
 
 max_time = 0 
 for i in range(0,len(input)):
     max_time = max(max_time, sum(all_drones[i].times) + (len(input)+1)*(travel_time + sensing_time)) 
     # This is wrong, it needs to add sensing and travel time too
 
+print("here")
 cf = swarm.allcfs.crazyflies[0]
 cf.takeoff(targetHeight=1.0, duration=2.5)
+timeHelper.sleep(7.5)
+print("here")
 
 # Cycle through the time slots
 # If a drone moves at that time slot, move it
@@ -71,14 +68,11 @@ while np.any(next_moves > -1):
 
     print("t=",t)
 
-    print(next_moves)
-
     for i in range(0,len(all_drones)):
 
         cf = all_drones[i]
         if next_moves[i] == 0: # if it's time for the drone to change status (i.e. it has finished its current task)
 
-            # Could change all_drones[i] to a variable drone=all_drones[i]. Would only work if implemented as a pointer, as otherwise the changes wouldn't be relayed back to all_drones
             # Could implement a move counter in Drone class e.g. drone.nmoves to track how many cells it had visited, then do drone.positions[nmoves] and times[nmoves] instead of costly(?) pop() operations
             # Sensing and waiting could technically be combined into one state of length (sensing_time + wait time) since they both currently just involve staying in the same spot, but have split them here to allow extension for different functionality in each state (e.g. to perform sensing actions) 
 
@@ -95,17 +89,19 @@ while np.any(next_moves > -1):
                 next_moves[i] = cf.times[0] +1
                 cf.times.pop(0)
 
+                """
                 # if wait time was 0 then need to move straight to moving in this iteration too
                 if next_moves[i] == 0:
                     # Literally just copy-pasted from below
                     cf.status = "moving"
-                    #cf.goTo(cf.positions[0], travel_time)
+                    cf.drone.goTo(position_to_coords[cf.positions[0]], 0, travel_time)
                     cf.positions.pop(0)
                     next_moves[i] = travel_time +1
+                """
 
             else: # cf.status == "waiting"
                 cf.status = "moving"
-                #cf.goTo(cf.positions[0], travel_time)
+                cf.drone.goTo(position_to_coords[cf.positions[0]], 0, travel_time)
                 cf.positions.pop(0)
                 next_moves[i] = travel_time +1
                 
@@ -114,8 +110,6 @@ while np.any(next_moves > -1):
                 if len(cf.times) < 1:  
                     # if all times are used up, then mark drone as done
                     next_moves[i] = -1
-
-        #print(i,":",cf.status)
 
     # minus 1 second from all next_moves (represents 1 second passing)
     next_moves = next_moves - np.full((1,len(all_drones)),1)[0]
