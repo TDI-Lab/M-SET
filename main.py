289e67104bc9e89d31822cd15e0958a6deb2be67
TszHo Wong <sc20thw@leeds.ac.uk>
2024-02-20 14:57:39 +0000

    'Hardcoded' the path to output config files

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 """
 Workflow executed from here.
 """
-import numpy as np
-
 from path_generation.PathGenerator import PathGenerator
 
 
 def main():
     pg = PathGenerator()
     plans = pg.generate_paths()
-    plan_total = np.array([0. for _ in range(len(plans[0][1]))])
-    for plan in plans:
-        print(plan)
-        plan_total += np.array(plan[1])
-    target_total = np.array([5., 9., 4., 5., 4., 8.])
-    print("Total Plans:", plan_total)
-    print("Mismatch:", plan_total - target_total)
+
+    sum = []
+    
+    if plans is None or len(plans) == 0:
+        print("No plans were generated.")
+    else:
+        for plan in plans:
+            print(plan)
+            # accumulate the time of each column (aka cell in the plan)
+            for i in range(len(plan[1])):
+                if i >= len(sum):
+                    sum.append(plan[1][i])
+                else:
+                    sum[i] += plan[1][i]
+    
+    # print the total time of the plan 
+    for i in range(len(sum)):
+        print("Total time of column", i, "is", sum[i])
+        
 
 
 if __name__ == '__main__':
--- path_generation/configManager.py
+++ path_generation/configManager.py
@@ -2,9 +2,10 @@
 
 import os
 import configparser
+from pathlib import Path
 
 # These lists are used to create the config file for the plan generation and EPOS
-# Everything in the lists are just for the example, the actual parameters should be defined in the global config file
+# Everything in the lists are just for the example, the actual parameters will be defined in the global config file
 sections_for_pathgen = [
     "plan",
     "map",
@@ -12,13 +13,15 @@ sections_for_pathgen = [
 ]
 
 sections_for_epos = [
-    "airSpeed"
+    "epos"
 ]
 
 class configManager:
     # initialize the class
     def __init__(self, config_path):
-        self.config_path = config_path
+        parent_path = Path(__file__).parent.resolve()
+        config_file = parent_path / config_path
+        self.config_path = config_file
         self.configs = configparser.ConfigParser()
         self.read_global_config()
 
@@ -28,33 +31,9 @@ class configManager:
             raise FileNotFoundError(f"The config file {self.config_file} does not exist.")
         self.config.read(self.config_file)
 
-        """
-        # This is the old way to read the global config file
-        # Since there is .properties file with different format, I will comment it out for now
-        try:
-            with open(config_file, "r") as f:
-                lines = f.readlines()
-                configs = {}
-                desc = []
-                for line in lines:
-                    stripped_line = line.strip()
-
-                    if stripped_line.startswith("# "):
-                        desc.append(line)
-                    elif "=" in stripped_line:
-                        key, value = stripped_line.split("=", 1)
-                        key = key.strip()
-                        value = value.strip()
-                        configs[key] = {"value": value, "description": desc}
-                        desc = []
-
-        except IOError as err:
-            print(f"An error occurred while reading the file: {err}")
-        """
-
     # get the value of a parameter from the global config file
-    # section: the section of the parameter
-    # option: the name of the parameter
+    # section: the section of the parameter, e.g. [plan], [map], [power], [epos]
+    # option: the name of the parameter, e.g. num_paths, map_file, power_file, etc.
     def get_property(self, section, option):
         return self.config.get(section, option)
     
@@ -64,67 +43,45 @@ class configManager:
         self.config.set(section, option, value)
         
     # create the config file for the plan generation
-    def write_pathgen_config_file(self, file_name = "pathGen_config.properties", storing_directory = "./"):
-        save_path = os.path.join(storing_directory, file_name)
-
-        # Filter the sections that are used for the path generation
-        save_content = configparser.ConfigParser()
-        for section in sections_for_pathgen:
-            if self.config.has_section(section):
-                save_content.add_section(section)
-                for key, value in self.config[section]:
-                    save_content.set(section, key, value)
-
-        with open(save_path, "w") as f:
-            save_content.write(f)
-
-
-
-        """
-        file_path = os.path.join(storing_directory, file_name)  # the path of the config file
+    def write_pathgen_config_file(self):
+        parent_path = Path(__file__).parent.resolve()
+        save_path = parent_path / 'PlanGeneration/conf/generation.properties'
 
         try:
-            with open(file_path, "w") as f:
-                f.write("###### Configuration file for the path generation ######\n\n")
-                for key in param_for_pathgen:
-                    for line in self.configs[key]["description"]:
-                        f.write(line)
-                    f.write(f'{key} = {self.configs[key]["value"]}\n\n')
-        except (IOError, KeyError) as err:
-            print(f"An error occurred while writing the file: {err}")
-            return None
-        """
+            # Filter the sections that are used for the path generation
+            save_content = configparser.ConfigParser()
+            for section in sections_for_pathgen:
+                if self.config.has_section(section):
+                    save_content.add_section(section)
+                    for key, value in self.config[section].items():
+                        save_content.set(section, key, value)
 
-    # create the config file for the plan generation, same as the previous function
-    def write_epos_config_file(self, file_name = "epos_config.properties", storing_directory = "./"):
-        save_path = os.path.join(storing_directory, file_name)  # the path of the config file
+            with open(save_path, "w") as f:
+                save_content.write(f)
+            f.close()
+        except Exception as e:
+            print(f"An error occurred while writing the config file: {str(e)}")
 
-        # Filter the sections that are used for the EPOS
-        save_content = configparser.ConfigParser()
-        for section in sections_for_epos:
-            if self.config.has_section(section):
-                save_content.add_section(section)
-                for key, value in self.config[section]:
-                    save_content.set(section, key, value)
+    # create the config file for the plan generation, but without the section headers
+    # In the global config file, the parameter is stored in the [epos] section, just to make it easier to read and recognize the parameters
+    def write_epos_config_file(self):
+        parent_path = Path(__file__).parent.resolve()
+        save_path = parent_path / 'EPOS/conf/epos.properties'
 
-        with open(save_path, "w") as f:
-            save_content.write(f)
-
-        """
         try:
-            with open(file_path, "w") as f:
-                f.write("###### Configuration file for EPOS ######\n\n")
-                for key in param_for_epos:
-                    for line in self.configs[key]["description"]:
-                        f.write(line)
-                    f.write(f'{key} = {self.configs[key]["value"]}\n\n')
-        except (IOError, KeyError) as err:
-            print(f"An error occurred while writing the file: {err}")
-            return None
-        """
+            # Filter the sections that are used for the EPOS
+            with open(save_path, "w") as f:
+                for section in sections_for_epos:
+                    if self.config.has_section(section):
+                        for key, value in self.config[section]:
+                            f.write(f'{key}={value}\n\n')       # no space is allowed between the key and the value
+            
+            f.close()
+        except Exception as e:
+            print(f"An error occurred while writing the config file: {str(e)}")
 
 
 # Example
 #config = configManager("generation.properties")
 #config.write_pathgen_config_file()
-#config.write_epos_config_file("hi.properties")
+#config.write_epos_config_file()
