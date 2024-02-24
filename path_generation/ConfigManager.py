729d07ef0489b29aa72b80de2c3827e055ce4f63
C41Underscore <callum.methold@sky.com>
2024-02-24 23:34:30 +0000

    config manager improved and battery capacity sensing mismatch experiment created

--- path_generation/ConfigManager.py
+++ path_generation/ConfigManager.py
@@ -3,6 +3,7 @@
 import os
 import configparser
 from pathlib import Path
+from typing import Dict
 
 # These lists are used to create the config file for the plan generation and EPOS
 # Everything in the lists are just for the example, the actual parameters will be defined in the global config file
@@ -16,14 +17,20 @@ sections_for_epos = [
     "epos"
 ]
 
-class configManager:
+
+class ConfigManager:
     # initialize the class
-    def __init__(self, config_path):
-        parent_path = Path(__file__).parent.resolve()
-        config_file = parent_path / config_path
-        self.config_path = config_file
-        self.configs = configparser.ConfigParser()
-        self.read_global_config()
+    def __init__(self, *args):
+        self.target_path = None
+        if len(args) > 1:
+            config_path = args[0]
+            parent_path = Path(__file__).parent.resolve()
+            config_file = parent_path / config_path
+            self.config_path = config_file
+            self.configs = configparser.ConfigParser()
+            self.read_global_config()
+        else:
+            self.config_path = None
 
     # read the global config file and store the configurations in a dictionary
     def read_global_config(self):
@@ -36,12 +43,30 @@ class configManager:
     # option: the name of the parameter, e.g. num_paths, map_file, power_file, etc.
     def get_property(self, section, option):
         return self.config.get(section, option)
-    
+
     def set_property(self, section, option, value):
         if not self.config.has_section(section):
             self.config.add_section(section)
         self.config.set(section, option, value)
-        
+
+    def set_target_path(self, path):
+        self.target_path = path
+
+    def write_config_file(self, properties: Dict[str, Dict[str, str]]):
+        new_config = Path(self.target_path)
+        new_config.parent.mkdir(parents=True, exist_ok=True)
+        with open(self.target_path, "w") as file:
+            for section, settings in properties.items():
+                #  Write property without a section
+                if not isinstance(settings, dict):
+                    file.writelines(f"{section}={settings}\n")
+                    continue
+                #  Write section
+                file.writelines(f"[{section}]\n")
+                for setting, value in settings.items():
+                    file.writelines(f"{setting}={value}\n")
+                file.writelines("\n")
+
     # create the config file for the plan generation
     def write_pathgen_config_file(self):
         parent_path = Path(__file__).parent.resolve()
@@ -74,14 +99,13 @@ class configManager:
                 for section in sections_for_epos:
                     if self.config.has_section(section):
                         for key, value in self.config[section]:
-                            f.write(f'{key}={value}\n\n')       # no space is allowed between the key and the value
-            
+                            f.write(f'{key}={value}\n\n')  # no space is allowed between the key and the value
+
             f.close()
         except Exception as e:
             print(f"An error occurred while writing the config file: {str(e)}")
 
-
 # Example
-#config = configManager("generation.properties")
-#config.write_pathgen_config_file()
-#config.write_epos_config_file()
+# config = configManager("generation.properties")
+# config.write_pathgen_config_file()
+# config.write_epos_config_file()
--- path_generation/PathGenerationResultsCollector.py
+++ path_generation/PathGenerationResultsCollector.py
@@ -1,18 +1,92 @@
 """
 Will eventually move these into a results factory when it's available.
 """
-from os import listdir
+from copy import deepcopy
+from distutils.dir_util import copy_tree
+from os import listdir, mkdir
+from os.path import isdir
 from pathlib import Path
 from pprint import pprint
+from shutil import copyfile, copy2
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import make_interp_spline
 
 from path_generation.PathGenerator import PathGenerator
+from path_generation.ConfigManager import ConfigManager
+from path_generation.PlanGeneration.PlanGenerator import PlanGenerator
 
 
 class PathGenerationResultsCollector:
+    PLAN_GENERATION_STANDARD_PROPERTIES = {
+        "plan": {
+            "dataset": "testbed",
+            "planNum": 32,
+            "agentsNum": 4,
+            "timeSlots": 24,
+            "maxVisitedCells": 6
+        },
+        "map": {
+            "stationsNum": 4,
+            "height": 1,
+            "mapLength": 4
+        },
+        "power": {
+            "batteryCapacity": 2700.,
+            "bodyMass": 0.027,
+            "batteryMass": 0.05,
+            "rotorNum": 4,
+            "rotorDia": 0.03,
+            "projectedBody": 0.0599,
+            "projectedBattery": 0.0037,
+            "powerEfficiency": 0.8,
+            "groundSpeed": 6.94,
+            "airDensity": 1.225,
+            "airSpeed": 8.5
+        }
+    }
+
+    EPOS_STANDARD_PROPERTIES = {
+        "dataset": "testbed",
+        "numSimulations": 1,
+        "numIterations": 32,
+        "numAgents": 4,
+        "numPlans": 128,
+        "numChildren": 2,
+        "planDim": 6,
+        "shuffle": 0,
+        "shuffle_file": "permutation.csv",
+        "numberOfWeights": 2,
+        "weightsString": "0.0,0.0",
+        "behaviours": "same",
+        "agentsBehaviourPath": "default",
+        "constraint": "SOFT",
+        "constraintPlansPath": "default",
+        "constraintCostsPath": "default",
+        "strategy": "never",
+        "periodically.reorganizationPeriod": 3,
+        "convergence.memorizationOffset": 5,
+        "globalCost.reductionThreshold": 0.5,
+        "strategy.reorganizationSeed": 0,
+        "globalSignalPath": "",
+        "globalCostFunction": "VAR",
+        "scaling": "STD",
+        "localCostFunction": "INDEX",
+        "logger.GlobalCostLogger": "true",
+        "logger.LocalCostMultiObjectiveLogger": "true",
+        "logger.TerminationLogger": "true",
+        "logger.SelectedPlanLogger": "true",
+        "logger.GlobalResponseVectorLogger": "true",
+        "logger.PlanFrequencyLogger": "true",
+        "logger.UnfairnessLogger": "true",
+        "logger.GlobalComplexCostLogger": "false",
+        "logger.WeightsLogger": "false",
+        "logger.ReorganizationLogger": "true",
+        "logger.VisualizerLogger": "false",
+        "logger.PositionLogger": "true",
+        "logger.HardConstraintLogger": "false"
+    }
 
     def __init__(self):
         self.parent_path = Path(__file__).parent.resolve()
@@ -46,7 +120,7 @@ class PathGenerationResultsCollector:
         reorganisations = []
         for line in results_data:
             formatted = [int(float(i)) for i in line.strip("\n").split(",")]
-            mean = sum(formatted[1:])/(len(formatted[1:]))
+            mean = sum(formatted[1:]) / (len(formatted[1:]))
             reorganisations.append(mean)
         reorganisations = np.array(reorganisations)
         # x = np.linspace(iterations.min(), iterations.max(), 1000)
@@ -70,11 +144,15 @@ class PathGenerationResultsCollector:
         for line in results:
             formatted = [float(i) for i in line.strip("\n").split(",")[3:]]
             formatted = list(filter(lambda x: x != float("inf") and x != float("-inf"), formatted))
-            mean = sum(formatted[1:]) / (len(formatted[1:]))
+            mean = sum(formatted) / (len(formatted))
             avg_costs.append(mean)
 
+        x = np.linspace(iterations.min(), iterations.max(), 1000)
+        x_y_spline = make_interp_spline(iterations, avg_costs)
+        y = x_y_spline(x)
+
         fig, ax = plt.subplots()
-        ax.plot(iterations, avg_costs)
+        ax.plot(x, y)
         ax.grid()
         ax.set_xlabel("Iterations")
         ax.set_ylabel("Global Cost")
@@ -90,11 +168,15 @@ class PathGenerationResultsCollector:
         for line in results:
             formatted = [float(i) for i in line.strip("\n").split(",")[3:]]
             formatted = list(filter(lambda x: x != float("inf") and x != float("-inf"), formatted))
-            mean = sum(formatted[1:]) / (len(formatted[1:]))
+            mean = sum(formatted) / (len(formatted))
             avg_costs.append(mean)
 
+        x = np.linspace(iterations.min(), iterations.max(), 1000)
+        x_y_spline = make_interp_spline(iterations, avg_costs)
+        y = x_y_spline(x)
+
         fig, ax = plt.subplots()
-        ax.plot(iterations, avg_costs)
+        ax.plot(x, y)
         ax.grid()
         ax.set_xlabel("Iterations")
         ax.set_ylabel("Average Local Cost")
@@ -121,9 +203,9 @@ class PathGenerationResultsCollector:
         changes_on_each_iteration = [[] for _ in range(len(runs))]
         for run, iterations in runs.items():
             run_changes = []
-            for i in range(1, len(iterations)-1):
+            for i in range(1, len(iterations) - 1):
                 changes = 0.
-                prev_iter = iterations[i-1][1:]
+                prev_iter = iterations[i - 1][1:]
                 cur_iter = iterations[i][1:]
                 for p, c in zip(prev_iter, cur_iter):
                     if p != c:
@@ -134,10 +216,10 @@ class PathGenerationResultsCollector:
         #  Compute the average change for each iteration
         final_changes = []
         for i in range(len(changes_on_each_iteration[0])):
-            avg = 100.*(sum([arr[i] for arr in changes_on_each_iteration])/len(changes_on_each_iteration))
+            avg = 100. * (sum([arr[i] for arr in changes_on_each_iteration]) / len(changes_on_each_iteration))
             final_changes.append(avg)
         #  Compute the maximum number of iterations
-        iterations = np.array([i for i in range(1, len(final_changes)+1)])
+        iterations = np.array([i for i in range(1, len(final_changes) + 1)])
         #  Plot!
         x = np.linspace(iterations.min(), iterations.max(), 1000)
         x_y_spline = make_interp_spline(iterations, final_changes)
@@ -150,6 +232,90 @@ class PathGenerationResultsCollector:
         ax.set_ylabel("% Change in Plans")
         ax.set_title("% Change in Plans Overtime")
         fig.savefig(f"{self.parent_path}/results/change_in_plans.png")
+
+    def experiment_plan_generation_fault(self):
+        #  Set target directories
+        parent_path = Path(__file__).parent.resolve()
+        testbed_dir = f"{parent_path}/PlanGeneration/datasets/testbed/"
+        plan_gen_config_target = f"/{parent_path}/PlanGeneration/conf/generation.properties"
+        epos_config_target = f"/{parent_path}/EPOS/conf/epos.properties"
+        config = ConfigManager()
+        #  Create testbed.csv
+        testbed = [
+            "type,id,x,y,z,value",
+            "SENSE,0,1,1,1,5",
+            "SENSE,1,2,1,1,9",
+            "SENSE,2,3,1,1,4",
+            "SENSE,3,1,2,1,5",
+            "SENSE,4,2,2,1,4",
+            "SENSE,5,3,2,1,8",
+            "BASE,0,0,0,0,0",
+            "BASE,1,4,0,0,0",
+            "BASE,2,4,3,0,0",
+            "BASE,3,0,3,0,0"
+        ]
+        with open(f"{testbed_dir}/testbed.csv", "w") as file:
+            for line in testbed:
+                file.write(f"{line}\n")
+        #  Ensure EPOS configuration is correct
+        config.set_target_path(epos_config_target)
+        epos_conf = deepcopy(self.EPOS_STANDARD_PROPERTIES)
+        epos_conf["numPlans"] = 32
+        epos_conf["globalSignalPath"] = "datasets/testbed/testbed.target"
+        epos_conf["globalCostFunction"] = "MIS"
+        config.write_config_file(epos_conf)
+        #  Set up config for plan generation
+        config.set_target_path(plan_gen_config_target)
+        #  Enumerate Experiments
+        experiment_configs = []
+        base_battery = 2700
+        for i in range(1, 100):
+            ec = deepcopy(self.PLAN_GENERATION_STANDARD_PROPERTIES)
+            ec["power"]["batteryCapacity"] = base_battery * i
+            experiment_configs.append(ec)
+        #  Run the experiment!
+        battery_value = []
+        avg_mismatch = []
+        for experiment in experiment_configs:
+            avg_error = 0.
+            repeats = 5
+            for i in range(0, repeats):
+                #  1. Write config file
+                config.write_config_file(experiment)
+                #  2. Generate plans
+                pg = PathGenerator()
+                results = pg.generate_paths()
+                # 3. Load target
+                target_file = f"{testbed_dir}/testbed.target"
+                goal = None
+                with open(target_file, mode="r", encoding="utf-8-sig") as file:
+                    goal = file.readlines()
+                goal = np.array([float(i) for i in goal[0].strip("\n").split(",")])
+                #  4. Calculate Mismatch
+                total = np.zeros(goal.shape)
+                for result in results:
+                    result = np.array(result[1])
+                    total += result
+                mismatch = sum(abs(goal - total))
+                avg_error += mismatch
+            avg_error /= repeats
+            avg_mismatch.append(avg_error)
+            battery_value.append(experiment["power"]["batteryCapacity"])
+        #  Plot!
+        battery_value = np.array(battery_value)
+        avg_mismatch = np.array(avg_mismatch)
+
+        x = np.linspace(battery_value.min(), battery_value.max(), 1000)
+        x_y_spline = make_interp_spline(battery_value, avg_mismatch)
+        y = x_y_spline(x)
+
+        fig, ax = plt.subplots()
+        ax.plot(x, y)
+        ax.grid()
+        ax.set_xlabel("Battery Capacity (J)")
+        ax.set_ylabel("Average Sensing Mismatch")
+        ax.set_title("Battery Related Sensing Mismatch")
+        fig.savefig(f"{self.parent_path}/results/battery_capacity_sensing_mismatch.png")
 
     def experiment_reorganisation_mismatch(self):
         pass
@@ -170,7 +336,7 @@ class PathGenerationResultsCollector:
         avg_sensing = avg_sensing[0]
         error = abs(target - avg_sensing)
         #  Generate x-axis
-        x = [i for i in range(1, len(avg_sensing)+1)]
+        x = [i for i in range(1, len(avg_sensing) + 1)]
         #  Plot!
         fig, ax = plt.subplots()
         ax.bar(x, target)
@@ -183,5 +349,5 @@ class PathGenerationResultsCollector:
 
 if __name__ == '__main__':
     pgr = PathGenerationResultsCollector()
-    # pgr.collect_results()
-    pgr.perform_experiments()
+    pgr.experiment_plan_generation_fault()
+    # pgr.perform_experiments()
