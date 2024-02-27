8366718be9761ce3485bc35e2247141f405163a3
C41Underscore <callum.methold@sky.com>
2024-02-27 12:13:41 +0000

    paths added to returned results

--- path_generation/PathGenerationController.py
+++ path_generation/PathGenerationController.py
@@ -39,8 +39,7 @@ class PathGenerationController:
         self.move_plans()
         return result_code
 
-    # Extract the results from the selected plans
-    def extract_results(self) -> List[Tuple[float, List[float]]]:
+    def __get_plan_indexes(self):
         # read selected-plan.csv from the first directory within the output directory
         results_dir = listdir(f"{self.parent_path}/EPOS/output")[0]
         selected_plans_file = f"{self.parent_path}/EPOS/output/{results_dir}/selected-plans.csv"
@@ -48,16 +47,38 @@ class PathGenerationController:
             lines = file.readlines()
         # extract plan indexes from the last line of the file, converting them from strings to integers
         plan_indexes = [int(index) for index in lines[-1].strip("\n").split(",")[2:]]
-        print(f"Selected plan indexes: {plan_indexes}")
+        return plan_indexes
 
+    def __extract_sensing_values_from_indexes(self, indexes):
+        #  Extract sensing values at each step of the path
         selected_plans = []
-        for i, index in enumerate(plan_indexes):
-            # read the plans from the dataset directory, extract the cost and plan as a tuple and append to selected plan
+        for i, index in enumerate(indexes):
             with open(f"{self.parent_path}/EPOS/datasets/{self.config.get('plan', 'dataset')}/agent_{i}.plans") as file:
                 plans = file.readlines()
-                selected_plan = plans[index-1].strip("\n")
+                selected_plan = plans[index - 1].strip("\n")
                 cost, plan = selected_plan.split(":")
                 cost = float(cost)
                 plan = [float(i) for i in plan.split(",")]
                 selected_plans.append((cost, plan))
         return selected_plans
+
+    def __extract_paths_from_indexes(self, indexes):
+        #  Extract sensing values at each step of the path
+        selected_paths = []
+        for i, index in enumerate(indexes):
+            with open(f"{self.parent_path}/EPOS/datasets/{self.config.get('plan', 'dataset')}/agent_{i}.paths") as file:
+                paths = file.readlines()
+                selected_path = paths[index - 1].split(",")
+                selected_paths.append(selected_path)
+        return selected_paths
+
+    def extract_results(self) -> List[Tuple[float, List[float], List[str]]]:
+        # Extract the results from the selected plans and paths
+        plan_indexes = self.__get_plan_indexes()
+        print(f"Selected plan indexes: {plan_indexes}")
+        selected_plans = self.__extract_sensing_values_from_indexes(plan_indexes)
+        selected_paths = self.__extract_paths_from_indexes(plan_indexes)
+        results = []
+        for plan, path in zip(selected_plans, selected_paths):
+            results.append((plan[0], plan[1], path))
+        return results
--- path_generation/PathGenerator.py
+++ path_generation/PathGenerator.py
@@ -23,11 +23,12 @@ class PathGenerator:
 
         #  Get results from file
         result = self._generation_manager.extract_results()
+        # result = self.convert_data_to_table(result)
         return result
 
     # Read the testbed mapping from a csv file
     # Extract the coordinate of the testbed from the file
-    def read_testbed_mapping(self):
+    def read_testbed_mapping(self, debug=False):
         parent_path = Path(__file__).parent.resolve()
         path_to_testbed = parent_path / 'PlanGeneration/datasets/testbed/testbed.csv'
 
@@ -35,7 +36,6 @@ class PathGenerator:
         with open(path_to_testbed, 'r') as f:
             next(f)  # Skip the first line
             for line in f:
-                print(line)
                 line = line.strip()
                 if len(line) == 0:  # make sure the line is not empty
                     continue
@@ -43,22 +43,27 @@ class PathGenerator:
                 parts = line.split(',')
                 object_type = parts[0].lower()
                 if object_type in mapping:
-                    mapping[object_type].append((int(parts[2]), int(parts[3])))
+                    loc_id = f"station{parts[1]}" if object_type == "base" else str(parts[1])
+                    x = float(parts[2])
+                    y = float(parts[3])
+                    z = float(parts[4])
+                    mapping[object_type].append({'id': loc_id, 'x': x, 'y': y, 'z': z})
 
-        # debug
-        for object_type in mapping:
-            for location in mapping[object_type]:
-                print(f"{object_type}: {location}")
-        # debug end
+        if debug:
+            for object_type in mapping:
+                for location in mapping[object_type]:
+                    print(f"{object_type}: {location}")
         return mapping
 
+    # Convert the plans to a 2D-array that consists of the location and the time
+    # aID\t(s)  0    1    2  ...
+    #       0   10,2 10,2 10,2
+    #       1   0,50 1,48 1,21
+    #       2   0,0   5,0 10,0  <- location of the agent at time t
     def convert_data_to_table(self, plans):
-        # Convert the plans to a 2D-array that consists of the location and the time
-        # aID\t(s)  0    1    2  ...
-        #       0   10,2 10,2 10,2
-        #       1   0,50 1,48 1,21
-        #       2   0,0   5,0 10,0  <- location of the agent at time t
+        sensing_map = self.read_testbed_mapping()
+        location_time = []
 
-        location_time = []  # 2D array
+
 
         return location_time
