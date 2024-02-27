d1bc503eb70db92be8869eea57fdc4c22b5c6e44
C41Underscore <callum.methold@sky.com>
2024-02-27 15:36:50 +0000

    more tests added for path conversion

--- path_generation/test/unit_tests.py
+++ path_generation/test/unit_tests.py
@@ -3,7 +3,7 @@ import unittest
 from path_generation.PathGenerator import PathGenerator
 
 
-class TestPathConverterSingleAgent(unittest.TestCase):
+class TestPathConverter(unittest.TestCase):
 
     def test_0s_plan(self):
         #  Arrange
@@ -12,7 +12,7 @@ class TestPathConverterSingleAgent(unittest.TestCase):
         #  Action
         result = pg.convert_data_to_table(plans)
         #  Assert
-        desired_result = [(0., 0., 0.), (0., 0., 0.)]
+        desired_result = [[(0., 0., 0.), (0., 0., 0.)]]
         self.assertEquals(result, desired_result)
 
     def test_choppy_plan(self):
@@ -23,9 +23,9 @@ class TestPathConverterSingleAgent(unittest.TestCase):
         result = pg.convert_data_to_table(plans)
         #  Assert
         desired_result = [
-            (0., 0., 0.), (1., 1., 1.), (1., 1., 1.), (1., 1., 1.), (1., 1., 1.), (1., 1., 1.), (1., 1., 1.),
-            (1., 1., 1.), (1., 1., 1.), (1., 1., 1.), (1., 1., 1.), (3., 1., 1.), (3., 1., 1.), (3., 1., 1.),
-            (3., 1., 1.), (3., 1., 1.), (3., 2., 1.), (0., 0., 0.)
+            [(0., 0., 0.), (1., 1., 1.), (1., 1., 1.), (1., 1., 1.), (1., 1., 1.), (1., 1., 1.), (1., 1., 1.),
+             (1., 1., 1.), (1., 1., 1.), (1., 1., 1.), (1., 1., 1.), (3., 1., 1.), (3., 1., 1.), (3., 1., 1.),
+             (3., 1., 1.), (3., 1., 1.), (3., 2., 1.), (0., 0., 0.)]
         ]
         self.assertEquals(result, desired_result)
 
@@ -37,7 +37,112 @@ class TestPathConverterSingleAgent(unittest.TestCase):
         result = pg.convert_data_to_table(plans)
         #  Assert
         desired_result = [
-            (4., 3., 0.), (3., 2., 1.), (2., 2., 1.), (1., 1., 1.),
-            (2., 1., 1.), (3., 1., 1.), (1., 2., 1.), (4., 3., 0.)
+            [(4., 3., 0.), (3., 2., 1.), (2., 2., 1.), (1., 1., 1.),
+             (2., 1., 1.), (3., 1., 1.), (1., 2., 1.), (4., 3., 0.)]
+        ]
+        self.assertEquals(result, desired_result)
+
+    def test_multiple_agents_0s_plan(self):
+        #  Arrange
+        pg = PathGenerator()
+        plans = [
+            (0.0, [0., 0., 0., 0., 0., 0.], ["station0", "station0"]),
+            (0.0, [0., 0., 0., 0., 0., 0.], ["station1", "station1"]),
+            (0.0, [0., 0., 0., 0., 0., 0.], ["station2", "station2"]),
+            (0.0, [0., 0., 0., 0., 0., 0.], ["station3", "station3"])
+        ]
+        #  Action
+        result = pg.convert_data_to_table(plans)
+        #  Assert
+        desired_result = [
+            [(0., 0., 0.), (0., 0., 0.)],
+            [(4., 0., 0.), (4., 0., 0.)],
+            [(4., 3., 0.), (4., 3., 0.)],
+            [(0., 3., 0.), (0., 3., 0.)]
+        ]
+        self.assertEquals(result, desired_result)
+
+    def test_multiple_agents_choppy_plan(self):
+        #  Arrange
+        pg = PathGenerator()
+        plans = [
+            (0.0, [10., 0., 5., 0., 0., 1.], ["station0", "0", "2", "5", "station0"]),
+            (0.0, [0., 3., 0., 5., 5., 0.], ["station1", "4", "3", "1", "station1"]),
+            (0.0, [0., 2., 10., 0., 4., 0.], ["station2", "2", "1", "4", "station2"]),
+            (0.0, [0., 0., 5., 0., 2., 1.], ["station3", "5", "4", "2", "station3"])
+            ]
+        #  Action
+        result = pg.convert_data_to_table(plans)
+        #  Assert
+        desired_result = [
+            [(0., 0., 0.),
+             (1., 1., 1.), (1., 1., 1.), (1., 1., 1.), (1., 1., 1.), (1., 1., 1.), (1., 1., 1.), (1., 1., 1.),
+             (1., 1., 1.), (1., 1., 1.), (1., 1., 1.),
+             (3., 1., 1.), (3., 1., 1.), (3., 1., 1.), (3., 1., 1.), (3., 1., 1.),
+             (3., 2., 1.),
+             (0., 0., 0.)],
+            [(4., 0., 0.),
+             (2., 2., 1.), (2., 2., 1.), (2., 2., 1.), (2., 2., 1.), (2., 2., 1.),
+             (1., 2., 1.), (1., 2., 1.), (1., 2., 1.), (1., 2., 1.), (1., 2., 1.),
+             (2., 1., 1.), (2., 1., 1.), (2., 1., 1.),
+             (4., 0., 0.)],
+            [(4., 3., 0.),
+             (3., 1., 1.), (3., 1., 1.), (3., 1., 1.), (3., 1., 1.), (3., 1., 1.), (3., 1., 1.), (3., 1., 1.),
+             (3., 1., 1.), (3., 1., 1.), (3., 1., 1.),
+             (2., 1., 1,), (2., 1., 1,),
+             (2., 2., 1.), (2., 2., 1.), (2., 2., 1.), (2., 2., 1.),
+             (4., 3., 0.)],
+            [(0., 3., 0.),
+             (3., 2., 1.),
+             (2., 2., 1.), (2., 2., 1.),
+             (3., 1., 1.), (3., 1., 1.), (3., 1., 1.), (3., 1., 1.), (3., 1., 1.),
+             (0., 3., 0.)]
+        ]
+        self.assertEquals(result, desired_result)
+
+    def test_multiple_agents_moving_plan(self):
+        #  Arrange
+        pg = PathGenerator()
+        plans = [
+            (0.0, [1., 1., 1., 1., 1., 1.], ["station2", "5", "4", "0", "1", "2", "3", "station2"]),
+            (0.0, [1., 1., 1., 1., 1., 1.], ["station1", "3", "2", "1", "4", "0", "5", "station1"]),
+            (0.0, [1., 1., 1., 1., 1., 1.], ["station0", "2", "1", "4", "5", "3", "0", "station0"]),
+            (0.0, [1., 1., 1., 1., 1., 1.], ["station3", "1", "5", "3", "0", "4", "2", "station3"])
+            ]
+        #  Action
+        result = pg.convert_data_to_table(plans)
+        #  Assert
+        desired_result = [
+            [(4., 3., 0.), (3., 2., 1.), (2., 2., 1.), (1., 1., 1.),
+             (2., 1., 1.), (3., 1., 1.), (1., 2., 1.), (4., 3., 0.)],
+            [(4., 0., 0.), (1., 2., 1.), (3., 1., 1.), (2., 1., 1.),
+             (2., 2., 1.), (1., 1., 1.), (3., 2., 1.), (4., 0., 0.)],
+            [(0, 0., 0.), (3., 1., 1.), (2., 1., 1.), (2., 2., 1.),
+             (3., 2., 1.), (1., 2., 1.), (1., 1., 1.), (0., 0., 0.)],
+            [(0., 3., 0.), (2., 1., 1.), (3., 2., 1.), (1., 2., 1.),
+             (1., 1., 1.), (2., 2., 1.), (3., 1., 1.), (0., 3., 0.)]
+        ]
+        self.assertEquals(result, desired_result)
+
+    def test_multiple_agents_different_length_plan(self):
+        #  Arrange
+        pg = PathGenerator()
+        plans = [
+            (0.0, [1., 1., 0., 0., 1., 1.], ["station2", "5", "4", "0", "1", "station2"]),
+            (0.0, [1., 1., 1., 1., 1., 0.], ["station1", "3", "2", "1", "4", "0", "station1"]),
+            (0.0, [0., 1., 1., 0., 1., 0.], ["station0", "2", "1", "4", "station0"]),
+            (0.0, [1., 1., 1., 1., 1., 1.], ["station3", "1", "5", "3", "0", "4", "2", "station3"])
+            ]
+        #  Action
+        result = pg.convert_data_to_table(plans)
+        #  Assert
+        desired_result = [
+            [(4., 3., 0.), (3., 2., 1.), (2., 2., 1.), (1., 1., 1.),
+             (2., 1., 1.), (4., 3., 0.)],
+            [(4., 0., 0.), (1., 2., 1.), (3., 1., 1.), (2., 1., 1.),
+             (2., 2., 1.), (1., 1., 1.), (4., 0., 0.)],
+            [(0, 0., 0.), (3., 1., 1.), (2., 1., 1.), (2., 2., 1.), (0., 0., 0.)],
+            [(0., 3., 0.), (2., 1., 1.), (3., 2., 1.), (1., 2., 1.),
+             (1., 1., 1.), (2., 2., 1.), (3., 1., 1.), (0., 3., 0.)]
         ]
         self.assertEquals(result, desired_result)
