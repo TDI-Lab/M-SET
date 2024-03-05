95155b27d29fe21465c160c7436432f6a53f1093
Hritik Mehta <sc19h2m@leeds.ac.uk>
2024-03-05 17:27:24 +0000

    Minor fix for script to work on different systems.

--- Hardware/cdca_epos_executor.py
+++ Hardware/cdca_epos_executor.py
@@ -1,13 +1,23 @@
 import numpy as np
 import os
-from pycrazyswarm import Crazyswarm
+import sys
 import math
+# print the original sys.path
+print('Original sys.path:', sys.path)
+
+# append a new directory to sys.path
+sys.path.append('/home/hritik/crazyswarm/ros_ws/src/crazyswarm/scripts')
+
+# print the updated sys.path
+print('Updated sys.path:', sys.path)
+
+from pycrazyswarm import Crazyswarm
 
 #input_path = [[[[0,0],3],[[1,1],6],[[0,1],0]],[[[2,1],3],[[1,0],12],[[2,0],0]]]
 
 # Parameters
-input_file_path="example_cdca_output.txt"
-crazyswarm_scripts_file_path="/home/adam/Documents/Packages/crazyswarm/ros_ws/src/crazyswarm/scripts"
+input_file_path="example-cdca-output.txt"
+crazyswarm_scripts_file_path="/home/hritik/crazyswarm/ros_ws/src/crazyswarm/scripts"
 all_drones = []
 next_moves = np.array([]) # Number of timeslots to next action, for each drone
 global_travel_time = 3 # Only used if travel_time_mode=0
