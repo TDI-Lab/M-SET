d11a28903722dfbd9b2ff56d3aff2f00a5051fac
Alex Robins <ed19ar3@leeds.ac.uk>
2024-04-13 12:50:52 +0100

    Added warning to swarm constants, to not set MINIMUM_DISTANCE to less than DISTANCE_STEP as this can cause CA strategies to not detect collisions

--- cdca/src/Swarm_Constants.py
+++ cdca/src/Swarm_Constants.py
@@ -1,12 +1,14 @@
 # Swarm constants definition.
-SPEED = 0.1
+
+
+SPEED = 1
 TIME_STEP = 1
 
 DISTANCE_STEP = SPEED * TIME_STEP
 
 TIME_DELAY = 2
 
-MINIMUM_DISTANCE = 0.5
+MINIMUM_DISTANCE = 1 # Must be >= to DISTANCE_STEP to ensure accuracy
 
 COLLISION_AVOIDANCE_LIMIT = 20
 
