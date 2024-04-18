ccacd6abae69f2a80cc8e7b7b4fe360f046b56b2
Alex Robins <ed19ar3@leeds.ac.uk>
2024-04-18 13:05:10 +0100

    updated experiments

--- experiments/VisualiseData.py
+++ experiments/VisualiseData.py
@@ -3,18 +3,27 @@ from matplotlib import pyplot as plt
 import pandas as pd
 
 class VisualiseData:
-    def __init__(self, path_to_results, path_to_save):
-        self.path_to_results = path_to_results
+    def __init__(self, paths_to_results, path_to_save):
+        self.path_to_results = paths_to_results
         self.path_to_save = path_to_save
         self.df = None
 
         self.read_results(self.path_to_results)
 
-    def read_results(self, path):
-        #use pandas to read csv file
-        self.df = pd.read_csv(path)
+    def read_results(self, paths):
 
-    def plotNumAgentsVsSensingAccuracy(self):
+        # If paths is a string, make it a one-item list
+        if isinstance(paths, str):
+            paths = [paths]
+        #use pandas to read csv file
+        for path in paths:
+            if self.df is None:
+                self.df = pd.read_csv(path)
+            else:
+                self.df = pd.concat([self.df, pd.read_csv(path)])
+        # self.df = pd.read_csv(path)
+
+    def plotNumAgentsVsSensingAccuracy(self, map_name):
         #plot a line graph where one axis is the number of agents and the other is the sensing accuracy
         # for each strategy
 
@@ -33,12 +42,12 @@ class VisualiseData:
             plt.plot(strategy_data['n_drones'], strategy_data['Sensing Mismatch %'], label=strategy)
 
         plt.xlabel('Number of Agents')
-        plt.ylabel('Sensing Accuracy (%)')
-        plt.title('Number of Agents vs Sensing Accuracy')
+        plt.ylabel('Sensing Mismatch (%)')
+        plt.title('Number of Agents vs Sensing Mismatch % for ' + map_name)
         plt.legend()
 
         #save the plot
-        plt.savefig(self.path_to_save + 'num_agents_vs_sensing_accuracy.png')
+        plt.savefig(self.path_to_save + 'num_agents_vs_sensing_accuracy_'+map_name+'.png')
         plt.show()
 
         
@@ -47,23 +56,33 @@ class VisualiseData:
         #plot a line graph where one axis is the number of agents and the other is the number of collisions
         # for each strategy
 
-        #group the data by strategy
-        grouped = self.df.groupby('Strategy')
+        #group the data by strategy and number of agents
+        grouped = self.df.groupby(['Strategy', 'n_drones'])
 
-        #plot the data
-        for name, group in grouped:
-            plt.plot(group['n_drones'], group['Total Collisions'], label=name)
+        #get mean of sensing accuracy for each num of agents per strategy
+        means = grouped['Total Collisions'].mean().reset_index()
+
+        #get different strategies
+        strategies = means['Strategy'].unique()
+
+        # plot the mean sensing accuracy for each num of agents, per strategy
+        for strategy in strategies:
+            strategy_data = means[means['Strategy'] == strategy]
+            plt.plot(strategy_data['n_drones'], strategy_data['Total Collisions'], label=strategy)
+
 
         plt.xlabel('Number of Agents')
         plt.ylabel('Number of Collisions')
         plt.title('Number of Agents vs Number of Collisions')
         plt.legend()
-        plt.show()
 
         #save the plot
         plt.savefig(self.path_to_save + 'num_agents_vs_collisions.png')
-        
+        plt.show()
 
+    def plotSensingMismatchVsCollisions():
+        pass
+    
 # Path: experiments/MeasureSensing.py
     
 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Workflow executed from here.
 """
 
 
+import copy
 from cdca.src.Input_Parser import Input_Parser
 from cdca.src.Potential_Fields_Collision_Avoidance import Potential_Fields_Collision_Avoidance
 from cdca.src.Swarm_Control import Swarm_Control
@@ -52,15 +53,15 @@ def write_results_to_csv(data, config, greedy=False):
 
         # Write the header if it doesn't exist
         if not header_exists:
-            writer.writerow(['Strategy', 'n_drones', 'Plan', 'Sensing Mismatch %','Total Collisions', 'Cross Collisions', 'Parallel_Collisions', 'Cell Occupied Collisions'])
+            writer.writerow(['Strategy', 'n_drones', 'Plan', 'Sensing Mismatch %','Total Collisions', 'Cross Collisions', 'Parallel_Collisions', 'Cell Occupied Collisions', 'Total Flights Distance', 'Total Collision Distance', 'Risk of Collision', 'Total Duration of Flights', 'Number of Flights', 'Average Collisions per Flight'])
         
         # Write the data
         for strategy, _ in data['plans'].items():
             collisions = data['results'][strategy]
-            sensing_accuracy = data['sensing mismatch'][strategy]
+            sensing_mismatch = data['sensing mismatch'][strategy]
             plans = data['plans'][strategy]
             # Join the plans into a single string
-            writer.writerow([strategy, n_drones, plans, sensing_accuracy, collisions['number_of_collisions'], collisions['number_of_cross_collisions'], collisions['number_of_dest_occupied_collisions']])
+            writer.writerow([strategy, n_drones, plans, sensing_mismatch, collisions['number_of_collisions'], collisions['number_of_cross_collisions'],collisions['number_of_parallel_collisions'], collisions['number_of_dest_occupied_collisions'], collisions['total_flights_distance'], collisions['total_collision_distance'], collisions['risk_of_collision'], collisions['total_duration_of_flights'], collisions['number_of_flights'], collisions['average_collisions_per_flight']])
         
         # Add a blank line for readability
         writer.writerow([])
@@ -92,8 +93,8 @@ def experiment_1and2_iteration(n_drones, mission_name):
     pg = PathGenerator()
     plans = pg.generate_paths()
 
-    for plan, path in plans.items():
-        print(f"plan: {plan}, path: {path}")
+    for plant, path in plans.items():
+        print(f"plan: {plant}, path: {path}")
 
     # if n_drones == 1:
     #     plans = [plans]
@@ -102,52 +103,59 @@ def experiment_1and2_iteration(n_drones, mission_name):
     parsed_plans = input_p.parsed_input
     
     print("")
-    for plan in parsed_plans:
-        print(plan)
+    for planx in parsed_plans:
+        print(planx)
     print("")
 
     sensing = MeasureSensing(f"examples/{mission_name}")
 
-    swarm_controller = Swarm_Control(parsed_plans, Potential_Fields_Collision_Avoidance()) # No collision avoidance actually being used here
-    swarm_controller2 = Swarm_Control(parsed_plans, Potential_Fields_Collision_Avoidance(visualise=False))
+    swarm_controller = Swarm_Control(parsed_plans, Potential_Fields_Collision_Avoidance())
+    swarm_controller2 = Swarm_Control(parsed_plans, Potential_Fields_Collision_Avoidance())
     swarm_controller3 = Swarm_Control(parsed_plans, Basic_Collision_Avoidance())
+
+    # priority_map = swarm_controller.determine_priority(parsed_plans)
+    # swarm_controller4 = Swarm_Control(parsed_plans, Basic_Collision_Avoidance(priority_map=priority_map))
 
     swarm_controller2.detect_potential_collisions()
     swarm_controller3.detect_potential_collisions()
+    # swarm_controller2.visualise_swarm()
+    # swarm_controller4.detect_potential_collisions()
 
-    plans =  [[plan for plan in drone.plan] for drone in swarm_controller.drones]
-    plans2 = [[plan for plan in drone.plan] for drone in swarm_controller2.drones]
-    plans3 = [[plan for plan in drone.plan] for drone in swarm_controller3.drones]
-    
-    print("Plans: ")
-    print("No CA: ",plans)
-    # print("PF CA: ",plans2)
-    print("Basic CA: ",plans3)
+
+    no_ca_plans =  [[plan1 for plan1 in drone.plan] for drone in swarm_controller.drones]
+    pf_plans = [[plan2 for plan2 in drone.plan] for drone in swarm_controller2.drones]
+    basic_plans = [[plan3 for plan3 in drone.plan] for drone in swarm_controller3.drones]
+    # priority_plans = [[plan4 for plan4 in drone.plan] for drone in swarm_controller4.drones]
+
     return {
     'plans': {
-        'no_ca':plans,
-        'pf_ca': plans2,
-        'basic_ca':plans3,
+        'no_ca':no_ca_plans,
+        'pf_ca': pf_plans,
+        'basic_ca':basic_plans,
+        # 'priority_ca': priority_plans,
     },
     'results': {
         'no_ca': swarm_controller.get_offline_collision_stats(),
         'pf_ca': swarm_controller2.get_offline_collision_stats(),
         'basic_ca': swarm_controller3.get_offline_collision_stats(),
+        # 'priority_ca': swarm_controller4.get_offline_collision_stats(),
     },
     'sensing mismatch': {
-        'no_ca': sensing.measure_sensing(plans),
-        'pf_ca': sensing.measure_sensing(plans2),
-        'basic_ca': sensing.measure_sensing(plans3),
+        'no_ca': sensing.measure_sensing(no_ca_plans),
+        'pf_ca': sensing.measure_sensing(pf_plans),
+        'basic_ca': sensing.measure_sensing(basic_plans),
+        # 'priority_ca': sensing.measure_sensing(priority_plans),
     }
 }
 def run_experiment_1and2():
      # a list of n m for each experiment grid
-    experiment_sizes = [[2,3], [4,4]]
+    experiment_sizes = [[2,3],[3,3],[4,4],[5,5],[6,6],[7,7],[8,8],[9,9],[10,10]]
 
     config = Config('drone_sense.properties')
 
-    n_iterations = 20
-    drones = [5,2,3,4,5,6,7,8]
+    n_iterations = 5
+    drones = [1,2,3,4,5,6,7,8]
+    # drones = [5,6,7,8]
 
     for i in range(len(experiment_sizes)):
         abs_path = os.path.abspath('.')
@@ -166,15 +174,25 @@ def run_experiment_1and2():
 
                 data = experiment_1and2_iteration(n_drones, mission_name)
                 write_results_to_csv(data, config)
+                
 
 
 if __name__ == '__main__':
-    # run_experiment_1and2()
+    run_experiment_1and2()
 
-
+    data_paths = ['experiments/results/random_2x3_results.csv','experiments/results/random_3x3_results.csv','experiments/results/random_4x4_results.csv','experiments/results/random_5x5_results.csv', 
+                  'experiments/results/random_6x6_results.csv', 'experiments/results/random_7x7_results.csv', 'experiments/results/random_8x8_results.csv']
+                #   'experiments/results/random_9x9_results.csv', 'experiments/results/random_10x10_results.csv']
+    data_paths = [ 'experiments/results/random_2x3_results.csv']
+    # map_name = "2x3"
     # Visualise the data
-    vd = VisualiseData('experiments/results/random_2x3_results.csv', 'experiments/results/')
-    vd.plotNumAgentsVsSensingAccuracy()
+    # for path in data_paths:
+        # print(path)
+        #extract the 5x5 from the path
+        # map_name = path.split('/')[-1].split('_')[1]
+        # print(map_name)
+    # vd = VisualiseData(data_paths, 'experiments/results/')
+    # vd.plotNumAgentsVsSensingAccuracy(map_name)
     # vd.plotNumAgentsVsCollisions()
 
    
