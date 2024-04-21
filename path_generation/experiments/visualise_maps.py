d87106a27d4d105c67b232376d00320df9a32fe9
C41Underscore <callum.methold@sky.com>
2024-04-21 22:28:26 +0100

    can generate and visualise pneuma maps

--- path_generation/experiments/visualise_maps.py
+++ path_generation/experiments/visualise_maps.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib as mlp
 from matplotlib import pyplot as plt
 import helper
 from path_generation.PathGenerator import PathGenerator
@@ -6,76 +7,53 @@ from path_generation.PathGenerator import PathGenerator
 figures = 1
 
 
-def visualise_map(size, map_type="regular"):
-    global figures
-    with open(f"{helper.get_parent_path()}/../../examples/{size}x{size}_{map_type}.csv", "r",
-              encoding="utf-8-sig") as file:
-        lines = file.readlines()
-    lines = lines[1:]
-    x_points = []
-    y_points = []
-    sizes = []
-    for line in lines:
-        line = line.strip("\n").split(",")
-        x = float(line[2])
-        y = float(line[3])
-        sensing_value = float(line[5]) * 10.
-        x_points.append(x)
-        y_points.append(y)
-        sizes.append(sensing_value)
-    plt.figure(figures)
-    figures += 1
-    plt.scatter(x_points, y_points, c=sizes, s=100., marker="s")
+desired_title = ""
+color_scheme = "viridis"
 
 
 def visualise_map_by_path(path):
     global figures
+    plt.figure(figures, figsize=(6, 6))
     with open(path, "r", encoding="utf-8-sig") as file:
         lines = file.readlines()
     lines = lines[1:]
     x_points = []
     y_points = []
     sizes = []
+    #  sensing points
     for line in lines:
         line = line.strip("\n").split(",")
         x = float(line[2])
         y = float(line[3])
         sensing_value = float(line[5])
-        x_points.append(x)
-        y_points.append(y)
-        sizes.append(sensing_value)
-    plt.figure(figures)
-    figures += 1
-    plt.scatter(x_points, y_points, c=sizes, s=50., marker="s")
-
-
-def visualise_testbed():
-    global figures
-    with open(f"{helper.get_parent_path()}/../../examples/testbed.csv", "r", encoding="utf-8-sig") as file:
-        lines = file.readlines()
-    lines = lines[1:]
+        if line[0] == "SENSE":
+            x_points.append(x)
+            y_points.append(y)
+            sizes.append(sensing_value)
+    plt.scatter(x_points, y_points, c=sizes, s=100., marker="s", cmap=color_scheme)
+    #  base stations
     x_points = []
     y_points = []
-    sizes = []
     for line in lines:
         line = line.strip("\n").split(",")
         x = float(line[2])
         y = float(line[3])
         sensing_value = float(line[5])
-        x_points.append(x)
-        y_points.append(y)
-        sizes.append(sensing_value)
-    plt.figure(figures)
+        if line[0] == "BASE":
+            x_points.append(x)
+            y_points.append(y)
+    plt.scatter(x_points, y_points, s=100., marker="^")
+    plt.title(f"Desired Drone Sensing ({desired_title})")
     figures += 1
-    plt.scatter(x_points, y_points, c=sizes, s=100., marker="s")
 
 
 def visualise_sensing():
+    global figures
+    plt.figure(figures, figsize=(6, 6))
     pg = PathGenerator()
     results = pg.generate_paths(raw=True)
     results = [np.array(result[1]) for result in results]
     final_sensing = sum(results)
-    print(final_sensing)
     requirements = pg.read_sensing_requirements()
     x_points = []
     y_points = []
@@ -83,16 +61,23 @@ def visualise_sensing():
     for cell, sensing in zip(requirements["sense"], final_sensing):
         x_points.append(float(cell["x"]))
         y_points.append(float(cell["y"]))
-        cs.append(float(sensing)*100.)
+        cs.append(float(sensing))
+    plt.scatter(x_points, y_points, c=cs, s=100., marker="s", cmap=color_scheme)
     #  Plot base stations
+    x_points = []
+    y_points = []
     for base in requirements["base"]:
         x_points.append(float(base["x"]))
         y_points.append(float(base["y"]))
-        cs.append(0.)
-    plt.scatter(x_points, y_points, c=cs, s=100., marker="s")
+    plt.scatter(x_points, y_points, s=100., marker="^", c="b")
+    plt.title(f"Actual Drone Sensing ({desired_title})")
+    figures += 1
 
 
 if __name__ == "__main__":
     # num = 12
-    visualise_map_by_path("/home/c41/Drones-Testbed/examples/pneuma_grid.csv")
+    vehicle_types = ["Bus", "Car", "HeavyVehicle", "MediumVehicle", "Motorcycle", "Taxi"]
+    desired_title = "Taxi"
+    visualise_map_by_path(f"/home/c41/Drones-Testbed/examples/pneuma_grid_{desired_title}.csv")
+    visualise_sensing()
     plt.show()
