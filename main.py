5d0f11ecbb8f9febb9b2836bbd404ce784dfd954
Adam <sc19ap>
2024-04-22 13:35:41 +0100

    Reintegrating cdca and movement code into main

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ Workflow executed from here.
 """
 
 from path_generation.PathGenerator import PathGenerator
-
+from path_generation.PathGenerator import PathGenerator
+from cdca.src.Input_Parser import Input_Parser
+from cdca.src.Swarm_Control import Swarm_Control
+from cdca.src.Basic_Collision_Avoidance import Basic_Collision_Avoidance
+from cdca.src.Dependency_Collision_Avoidance import Dependency_Collision_Avoidance
+from Hardware.cdca_epos_executor import main as hardware_main
 
 def main():
-    raw = True
+    raw = False
     #  Hello :)  There are two steps to running the path generator (once the config is set up)
     #  First, instantiate the PathGenerator object
     pg = PathGenerator()
@@ -20,6 +25,20 @@ def main():
             print(plan, path)
     print(pg.get_coordinate_position_results())
 
+    input_p = Input_Parser(plans)
+    parsed_plans = input_p.parsed_input
+    
+    print("")
+    for plan in parsed_plans:
+        print(plan)
+    print("")
+
+    swarm_controller = Swarm_Control(parsed_plans, Basic_Collision_Avoidance())
+    swarm_controller.detect_potential_collisions()
+    print("final plan: \n", parsed_plans)
+    
+    #swarm_controller.plans =  execute these plans with hardware/simulation
+    hardware_main(swarm_controller.plans, raw=True)
 
 if __name__ == '__main__':
     main()
