91f4749651210627de4d6b7eb98f5285132322d9
Adam <sc19ap>
2024-04-28 23:09:09 +0100

    Saving changes made to configs used in hardware experiments

--- Hardware/Hardware_constants.py
+++ Hardware/Hardware_constants.py
@@ -4,11 +4,11 @@ SPEED = 0.1 #m/s
 INPUT_MODE = "cdca" # CAN BE OVERWITTEN AT COMMAND LINE # "cdca" or "default"
 ENABLE_LOGGING = True
 PRINT_LOG_MESSAGES= True
-LOG_OUTPUT_FILE = "eval1drone"
-IN_SIMULATION = True # Should be set automatically by --sim in command line
+LOG_OUTPUT_FILE = "placeholder"
+IN_SIMULATION = False # Should be set automatically by --sim in command line
 TRAVEL_TIME_MODE = 2 # REMOVE
-USE_CELL_COORDS = True # True=path uses testbed grid coords, False=path uses the actual positions from the physical testing environment
-TIMESTEP_LENGTH = 0.015625 # Should always be a value that can be represented by 2^{x}, where x is an integer
+USE_CELL_COORDS = False # True=path uses testbed grid coords, False=path uses the actual positions from the physical testing environment
+TIMESTEP_LENGTH = 0.015625#0.0078125 # Should always be a value that can be represented by 2^{x}, where x is an integer
 GLOBAL_TRAVEL_TIME = 6 # s, Not required unless TRAVEL_TIME_MODE=0
 SENSING_TIME=1 # s
 CRAZYSWARM_SCRIPTS_FILE_PATH = "/home/adam/Documents/Packages/crazyswarm/ros_ws/src/crazyswarm/scripts" # Needs to be set on the individual system
