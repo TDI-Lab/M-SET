f56ee7585bf9d4fefe19698c91eaeba4f39280c2
Adam <sc19ap>
2024-04-28 18:42:32 +0100

    Plotted basic energy consumption vs. no. drones results

--- Hardware/logTranslator_eval.py
+++ Hardware/logTranslator_eval.py
@@ -4,6 +4,7 @@ import fnmatch
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+from battery_calc import read_default_output, read_cdca_output, calc_total_times, calc_total_energy, calc_energy_consumption
 #working_path = "/content/drive/My Drive/bio-inspired"
 
 def format_data(in_filename, out_filename):
@@ -188,7 +189,7 @@ def calc_average_vloss(min_count, max_count, avg_lost, total_voltages_lost, divi
 
     return avg_lost
 
-def calculate_voltage_metrics(V, total_voltages_lost, avg_lost, divisor, counts, voltages_lost, count_end_times, min_count, max_count):
+def calculate_voltage_metrics(drones, V, total_voltages_lost, avg_lost, divisor, counts, voltages_lost, count_end_times, min_count, max_count):
     for count in range(min_count, max_count):
         flight_time = count-min_count
 
@@ -235,10 +236,12 @@ def get_total_power(avg_current, total_voltages_lost):
 
 def get_total_energy(avg_current,total_voltages_lost):
     Ps = get_total_power(avg_current, total_voltages_lost)
+    print(Ps)
+    print(sum(Ps))
     Energy = -sum(Ps)
     return Energy
 
-def batch_cacl_total_energy(filenames, drones_list):
+def batch_recorded_total_energy(filenames, drones_list):
     total_energy = []
 
     for i in range(0,len(filenames)):
@@ -264,7 +267,7 @@ def batch_cacl_total_energy(filenames, drones_list):
         max_count = int(max(count_end_times)) # latest flight start
 
         V, total_voltages_lost, avg_lost, divisor = initialise_voltage_metrics(min_count,max_count)
-        V, total_voltages_lost, avg_lost, divisor = calculate_voltage_metrics(V, total_voltages_lost, avg_lost, divisor, counts, voltages_lost, count_end_times, min_count, max_count)
+        V, total_voltages_lost, avg_lost, divisor = calculate_voltage_metrics(drones, V, total_voltages_lost, avg_lost, divisor, counts, voltages_lost, count_end_times, min_count, max_count)
 
         #plot_flighttime_voltageloss(drones,min_count,max_count,avg_lost,V)
 
@@ -273,6 +276,133 @@ def batch_cacl_total_energy(filenames, drones_list):
         total_energy.append(total_path_energy)
 
     return total_energy
+
+def batch_predicted_total_energy(ndrones, path_filenames, input_modes, speeds, avg_current):
+    # Each file has a different number of drones in it
+    
+    voltage_poly_coeff = []
+    #voltage_poly_coeff=[-4.60990873e-10, 1.76874340e-07, -1.85072541e-05, -1.00546782e-03, -9.19204536e-03] # moving
+    voltage_poly_coeff = [-3.54280460e-11,  1.14158206e-08,  2.20841775e-06, -2.14997073e-03, -3.66082298e-02] # hovering
+    #voltage_poly_coeff = [-1.77768893e-08,  9.93408521e-06, -2.85530205e-03, -2.22369528e-02] # hovering, deg=3
+    #voltage_poly_coeff = [-5.04951815e-13,  4.84672324e-10, -1.78941756e-07,  3.15654831e-05,-3.86950027e-03, -1.33939207e-02]
+    #voltage_poly_coeff = [ -2.59269349e-12, -1.80094258e-08,  -8.52202375e-06, 2.23200514e-03,  -3.57061644e+00]# raw values, as opposed to loss
+    #voltage_poly_coeff = [ -2.59269349e-12, 1.80094258e-08,  -8.52202375e-06, 2.23200514e-03, -3.57061644e+00]
+
+    model_total_energy = []
+    epos_total_energy = []
+
+    for i in range(0,len(path_filenames)):
+        print(path_filenames[i], input_modes[i])
+        if input_modes[i] == "default":
+            path = read_default_output(path_filenames[i])
+        elif input_modes[i] == "cdca":
+            path = read_cdca_output(path_filenames[i]) 
+
+        path = path[:ndrones[i]]
+        hover_time, flight_time, total_time = calc_total_times(path, input_modes[i], speeds[i])
+
+        # Hardare model
+        model_total_path_energy = calc_energy_consumption(total_time,"polynomial",voltage_poly_coeff,avg_current)
+
+        # EPOS model
+        epos_total_path_energy = epos_calc_predicted_energy(path,input_modes[i])[2]
+
+        model_total_energy.append(model_total_path_energy)
+        epos_total_energy.append(epos_total_path_energy)
+
+    return epos_total_energy, model_total_energy
+
+def epos_calc_predicted_energy(path, input_mode):
+    hover_power = 1.7583430197900498#1.125339532665632#1.6152318136132513#1.5308683747745433# 1.1297552885300068 # W
+    flight_power = hover_power # 1.128288294545468 # W
+    
+    #path = [[[[0.0, 0.0], 1], [[1.0, 1.0], 4], [[1.0, 2.0], 4], [[2.0, 1.0], 8], [[0.0, 0.0], 1]], [[[4.0, 0.0], 2]], [[[4.0, 3.0], 1], [[3.0, 2.0], 9], [[2.0, 2.0], 4], [[3.0, 1.0], 4], [[4.0, 3.0], 1]], [[[0.0, 3.0], 1], [[1.0, 2.0], 1], [[2.0, 2.0], 0], [[2.0, 1.0], 0], [[1.0, 1.0], 1], [[0.0, 3.0], 1]]]
+    speed = 0.1
+    avg_current = 3.75
+
+    hover_time, flight_time, total_time = calc_total_times(path, input_mode, speed)
+    hover_energy, flight_energy, total_energy = calc_total_energy(hover_power,flight_power,hover_time,flight_time)
+
+    return hover_energy, flight_energy, total_energy
+
+def plot_all_results():
+    # All on one
+    plt.plot(x,recorded_basic_energy_consumptions, label="Recorded: Basic cdca")
+    plt.plot(x,recorded_pf_energy_consumptions, label="Recorded: Potenial Fields cdca")
+    plt.plot(x,epos_predicted_basic_energy_consumptions, label="Predicted by EPOS: Basic cdca")
+    plt.plot(x,model_predicted_basic_energy_consumptions, label="Predicted by model: Basic cdca")
+    plt.plot(x,epos_predicted_pf_energy_consumptions, label="Predicted by EPOS: Potential Fields cdca")
+    plt.plot(x,model_predicted_pf_energy_consumptions, label="Predicted by model: Potential Fields cdca")
+    plt.plot(x,epos_predicted_nocdca_energy_consumptions, label="Predicted by EPOS: No cdca")
+    plt.plot(x,model_predicted_nocdca_energy_consumptions, label="Predicted by model: No cdca")
+    plt.xlabel("Number of drones in path")
+    plt.ylabel("Energy Consumption (J)")
+    plt.xticks([1,2,3,4])
+    plt.legend()
+    plt.show()
+
+def plot_by_cdca_type():
+    # Compare (recording, epos, model), for each cdca type
+
+    fig, axs = plt.subplots(1,2)
+
+    axs[0].plot(x,recorded_basic_energy_consumptions, label="Recorded: Basic cdca")
+    axs[0].plot(x,epos_predicted_basic_energy_consumptions, label="Predicted by EPOS: Basic cdca")
+    axs[0].plot(x,model_predicted_basic_energy_consumptions, label="Predicted by model: Basic cdca")
+
+    axs[1].plot(x,recorded_pf_energy_consumptions, label="Recorded: Potenial Fields cdca")
+    axs[1].plot(x,epos_predicted_pf_energy_consumptions, label="Predicted by EPOS: Potential Fields cdca")
+    axs[1].plot(x,model_predicted_pf_energy_consumptions, label="Predicted by model: Potential Fields cdca")
+
+    # axs[2].plot(x,epos_predicted_nocdca_energy_consumptions, label="Predicted by EPOS: No cdca")
+    # axs[2].plot(x,model_predicted_nocdca_energy_consumptions, label="Predicted by model: No cdca")
+
+    titles = ["Basic cdca","Potential Fields cdca"]
+    for c, ax in enumerate(axs):
+        ax.set_xlabel("Number of drones in path")
+        ax.set_ylabel("Energy Consumption (J)")
+        ax.set_xticks([1,2,3,4])
+        ax.set_ylim(0,1000)
+        ax.legend()
+        plt.title(titles[c])
+    plt.suptitle("Recorded vs. predicted values for each cdca type")
+    plt.show()
+
+def plot_by_value_origin():
+    # Compare (no cdca, basic, pf), for each (recording, epos, model)
+
+    fig, axs = plt.subplots(1,2)
+
+    # axs[0].plot(x,recorded_basic_energy_consumptions, label="Recorded: Basic cdca")
+    # axs[1].plot(x,recorded_pf_energy_consumptions, label="Recorded: Potenial Fields cdca")
+    
+    axs[0].plot(x,epos_predicted_nocdca_energy_consumptions, label="Predicted by EPOS: No cdca")
+    axs[0].plot(x,epos_predicted_basic_energy_consumptions, label="Predicted by EPOS: Basic cdca")
+    axs[0].plot(x,epos_predicted_pf_energy_consumptions, label="Predicted by EPOS: Potential Fields cdca")
+
+    axs[1].plot(x,model_predicted_nocdca_energy_consumptions, label="Predicted by model: No cdca")
+    axs[1].plot(x,model_predicted_basic_energy_consumptions, label="Predicted by model: Basic cdca")
+    axs[1].plot(x,model_predicted_pf_energy_consumptions, label="Predicted by model: Potential Fields cdca")
+
+    titles = ["Predicted by EPOS","Predicted by Model"]
+    for c, ax in enumerate(axs):
+        ax.set_xlabel("Number of drones in path")
+        ax.set_ylabel("Energy Consumption (J)")
+        ax.set_xticks([1,2,3,4])
+        ax.set_ylim(0,1000)
+        ax.legend()
+        plt.title(titles[c])
+    plt.suptitle("Predicted values for each cdca method, for each prediction method")
+    plt.show()
+
+def plot_nocdca_epos_vs_model():
+    plt.plot(x,epos_predicted_nocdca_energy_consumptions, label="Predicted by EPOS: No cdca")
+    plt.plot(x,model_predicted_nocdca_energy_consumptions, label="Predicted by model: No cdca")
+    plt.xlabel("Number of drones in path")
+    plt.ylabel("Energy Consumption (J)")
+    plt.xticks([1,2,3,4])
+    plt.legend()
+    plt.show()
 
 if __name__=="__main__":
     # in_filename = "Hardware/Hardware Results/eval_4dronescdca_manual2"
@@ -304,10 +434,35 @@ if __name__=="__main__":
     # total_path_energy = get_total_energy(avg_current, total_voltages_lost)
 
     drones = [[1],[1,2],[1,2,3],[1,2,3,4]]
-    filenames = [["Hardware/Hardware Results/eval_1dronescdca_manual","Hardware/Hardware Results/dataReformatted.txt"],["Hardware/Hardware Results/eval_2dronescdca_manual","Hardware/Hardware Results/dataReformatted.txt"],["Hardware/Hardware Results/eval_3dronescdca_manual","Hardware/Hardware Results/dataReformatted.txt"],["Hardware/Hardware Results/eval_4dronescdca_manual2","Hardware/Hardware Results/dataReformatted.txt"]]
-    energy_consumptions = batch_cacl_total_energy(filenames, drones)
+    basic_cdca_filenames = [["Hardware/Hardware Results/eval_1dronescdca_manual","Hardware/Hardware Results/dataReformatted.txt"],["Hardware/Hardware Results/eval_2dronescdca_manual","Hardware/Hardware Results/dataReformatted.txt"],["Hardware/Hardware Results/eval_3dronescdca_manual","Hardware/Hardware Results/dataReformatted.txt"],["Hardware/Hardware Results/eval_4dronescdca_manual2","Hardware/Hardware Results/dataReformatted.txt"]]
+    pf_cdca_filenames = [["Hardware/Hardware Results/eval_1dronescdca_manual","Hardware/Hardware Results/dataReformatted.txt"],["Hardware/Hardware Results/pf_2drones_cdca","Hardware/Hardware Results/dataReformatted.txt"],["Hardware/Hardware Results/pf_3drones_cdca","Hardware/Hardware Results/dataReformatted.txt"],["Hardware/Hardware Results/pf_4drones_cdca2","Hardware/Hardware Results/dataReformatted.txt"]]    
+    basic_cdca_path_filenames = []
+    pf_cdca_path_filenames = []
+    
+    # note that the pf cdca 1 drone is just the same as the basic cdca 1 drone, since no cdca should be applied with only 1 drone
+    recorded_basic_energy_consumptions = batch_recorded_total_energy(basic_cdca_filenames, drones)
+    recorded_pf_energy_consumptions = batch_recorded_total_energy(pf_cdca_filenames, drones)
+
+    # note that the pf cdca 1 drone is just the same as the basic cdca 1 drone, since no cdca should be applied with only 1 drone
+    basic_path_filenames = ["Hardware/Experiments/Working/manual/eval_1dronecdca.txt", "Hardware/Experiments/Working/manual/eval_2dronecdca.txt", "Hardware/Experiments/Working/manual/eval_3dronecdca.txt", "Hardware/Experiments/Working/manual/eval_4dronecdca.txt"]
+    pf_path_filenames = ["Hardware/Experiments/Working/manual/eval_1dronecdca.txt","Hardware/Experiments/Potential fields/pf_cdca_2drones.txt","Hardware/Experiments/Potential fields/pf_cdca_3drones.txt","Hardware/Experiments/Potential fields/pf_cdca_4drones2.txt"]
+    input_modes = ["cdca","cdca","cdca","cdca"]
+    speeds = [0.1,0.1,0.1,0.1]
+    avg_current = 3.75
+    epos_predicted_basic_energy_consumptions, model_predicted_basic_energy_consumptions = batch_predicted_total_energy([4,4,4,4], basic_path_filenames, input_modes, speeds, avg_current)
+    epos_predicted_pf_energy_consumptions, model_predicted_pf_energy_consumptions = batch_predicted_total_energy([4,4,4,4], pf_path_filenames, input_modes, speeds, avg_current)
+
+    nocdca_path_filenames = ["Hardware/Experiments/Working/manual/eval_4droneparsed.txt","Hardware/Experiments/Working/manual/eval_4droneparsed.txt","Hardware/Experiments/Working/manual/eval_4droneparsed.txt","Hardware/Experiments/Working/manual/eval_4droneparsed.txt"]
+    epos_predicted_nocdca_energy_consumptions, model_predicted_nocdca_energy_consumptions = batch_predicted_total_energy([1,2,3,4], nocdca_path_filenames, input_modes, speeds, avg_current)
 
     x = [len(d) for d in drones]
-    y = energy_consumptions
-    print(x,y)
-    plt.plot(x,y)
+    print("")
+    print(x, recorded_basic_energy_consumptions)
+    print(x, recorded_pf_energy_consumptions)
+    print(x, epos_predicted_basic_energy_consumptions)
+    print(x, model_predicted_basic_energy_consumptions)
+
+    plot_all_results()
+    plot_by_cdca_type()
+    plot_by_value_origin()
+    plot_nocdca_epos_vs_model()
