4d752eef9ca8ed0b7d791eeff71ce136d7f8fd91
Adam <sc19ap>
2024-04-30 23:58:32 +0100

    Manually recalculated paths

--- eval_experiments.py
+++ eval_experiments.py
@@ -26,15 +26,21 @@ def generate_epos():
 
     return plans
 
-def apply_cdca(parsed_plans):
-    swarm_controller = Swarm_Control(parsed_plans, Basic_Collision_Avoidance())
+def apply_cdca(parsed_plans, cdca_type):
+    if cdca_type == "basic":
+        swarm_controller = Swarm_Control(parsed_plans, Basic_Collision_Avoidance())
+    elif cdca_type == "pf":
+        swarm_controller = Swarm_Control(parsed_plans, Potential_Fields_Collision_Avoidance())
+    #print('***********************parsed_plans: ', parsed_plans)
     swarm_controller.detect_potential_collisions()
-
-    return swarm_controller.plans
+    new_pl = swarm_controller.plans
+    #print('*********************new plans: ', new_pl)
+    return new_pl
 
 if __name__ == "__main__":
+    """
     os.chdir(original_cwd)
-    output_file = "evaluation_paths.txt"
+    output_file = "pf_evaluation_paths.txt"
     try:
         file = open("Hardware/Experiments/%s" % output_file, "w")
     except:
@@ -51,6 +57,8 @@ if __name__ == "__main__":
     input_p = Input_Parser(plans)
     parsed_plans = input_p.parsed_input
 
+    #parsed_plans = [[[[0.0, 0.0], 1], [[1.0, 1.0], 25], [[2.0, 1.0], 3], [[1.0, 2.0], 25], [[0.0, 0.0], 1]], [[[4.0, 0.0], 1], [[3.0, 1.0], 38], [[2.0, 1.0], 7], [[3.0, 2.0], 7], [[4.0, 0.0], 1]], [[[0.0, 3.0], 1], [[1.0, 2.0], 22], [[2.0, 2.0], 8], [[1.0, 1.0], 22], [[0.0, 3.0], 1]], [[[4.0, 3.0], 1], [[3.0, 2.0], 6], [[3.0, 1.0], 30], [[2.0, 2.0], 18], [[4.0, 3.0], 1]]]
+
     file.write(str(parsed_plans))
     file.write("\n")
 
@@ -58,7 +66,7 @@ if __name__ == "__main__":
 
     # Generate the 1, 2, 3, 4 drone cdca paths
     print("")
-    for n_drones in range(5,1,-1):
+    for n_drones in range(4,0,-1):
         print("%s drones" % n_drones)
         cdca_plans = apply_cdca(parsed_plans[:n_drones])
         print(cdca_plans)
@@ -69,3 +77,12 @@ if __name__ == "__main__":
         #swarm_controller.plans =  execute these plans with hardware/simulation
         hardware_main(cdca_plans, raw=True, input_mode="cdca")
         print("")
+
+    """
+
+    parsed_plans = [[[[-0.8299, -0.47], 1], [[-0.5533, -0.235], 1], [[-0.5533, 0.235], 1], [[0.0, -0.235], 2], [[-0.8299, -0.47], 1]], [[[0.8299, -0.47], 1], [[0.5533, 0.235], 1], [[0.0, -0.235], 2], [[0.8299, -0.47], 1]], [[[0.8299, 0.47], 1], [[0.5533, 0.235], 1], [[0.0, -0.235], 2], [[0.8299, 0.47], 1]], [[[-0.8299, 0.47], 1], [[-0.5533, 0.235], 1], [[-0.5533, -0.235], 1], [[0.0, -0.235], 2], [[-0.8299, 0.47], 1]]]
+    parsed_plans = parsed_plans[:1]
+    print(parsed_plans)
+    print("")
+    cdca_plans = apply_cdca(parsed_plans, "pf")
+    print(cdca_plans)
