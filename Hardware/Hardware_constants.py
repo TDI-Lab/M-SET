bfc64435924e1478e1d453422ea3281e8d2e15a9
Adam <sc19ap>
2024-06-14 18:19:20 +0100

    Setting default values for Hardware config, and adding more comments explaining config options

--- Hardware/Hardware_constants.py
+++ Hardware/Hardware_constants.py
@@ -2,7 +2,7 @@
 HOVER_HEIGHT = 0.6 #m
 SPEED = 0.1 #m/s
 INPUT_MODE = "cdca" # CAN BE OVERWITTEN AT COMMAND LINE # "cdca" or "default"
-ENABLE_LOGGING = True
+ENABLE_LOGGING = False
 PRINT_LOG_MESSAGES= True
 LOG_OUTPUT_FILE = "placeholder"
 IN_SIMULATION = False # Set automatically by --sim in command line
@@ -11,4 +11,4 @@ USE_CELL_COORDS = False # True=path uses testbed grid coords, False=path uses th
 TIMESTEP_LENGTH = 0.015625#0.0078125 # Should always be a value that can be represented by 2^{x}, where x is an integer
 GLOBAL_TRAVEL_TIME = 6 # s, Not required unless TRAVEL_TIME_MODE=0
 SENSING_TIME=1 # s
-CRAZYSWARM_SCRIPTS_FILE_PATH = "crazyswarm/ros_ws/src/crazyswarm/scripts" # Needs to be set on the individual system
+CRAZYSWARM_SCRIPTS_FILE_PATH = "crazyswarm/ros_ws/src/crazyswarm/scripts" # Set to the full filepath (from root) of the crazyswarm/ros_ws/src/crazyswarm/scripts file in the crazyswarm installation. Needs to be set on each individual installation.
